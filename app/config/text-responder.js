06e06ea0b32076f4f9fe80de2f48cb3fb2eb308a
Jihdeh <jide.b.tade@gmail.com>
2016-09-15 16:18:02 +0100

    capture messages before they even go up to listerner

--- app/config/text-responder.js
+++ app/config/text-responder.js
@@ -6,22 +6,18 @@ import keywords from "../util/keywords";
 import sendTextSeachResult from "./google-text-search/template";
 
 async function listener(text, recipientId) {
-  if (recipientId) {
-    if (/[keywords.show]/i.test(text)) {
-      destructureText(text).then(response => {
-        console.log(response)
-        if(response) {
-          sendTextSeachResult(response, recipientId);
-        } else {
-          sendMessage(recipientId, `
-            Arggh sadly your search returned no results \n\nWhat if you try your search and you add your state/country? after? \n\nFor Example: \nshow me party clubs newyork \nor \nshow me shaunz bar lagos`);
-        }
-      });
+  destructureText(text).then(response => {
+    console.log(response)
+    if (response) {
+      sendTextSeachResult(response, recipientId);
     } else {
-      return sendMessage(recipientId, `Sorry command on show is incorrect, please check page`);
+      sendMessage(recipientId, `
+            Arggh sadly your search returned no results \n\nWhat if you try your search and you add your state/country? after? \n\nFor Example: \nshow me party clubs newyork \nor \nshow me shaunz bar lagos`);
     }
-  }
+  });
 }
+
+console.log(/shw me/i.test("show me restaurants in yaba nigeria"))
 
 // listener("show me restaurants in yaba nigeria");
 
@@ -33,7 +29,6 @@ async function destructureText(text) {
 }
 
 async function composeText(keyword, searchTerm) {
-  console.log(keyword, "word")
   switch (keyword) {
     case "show me":
       return await textSearch(searchTerm);
@@ -54,7 +49,12 @@ async function sendTextMessage(recipientId, messageText, postback) {
     } else if (genericResponse.byes.includes(messageText)) {
       return sendMessage(recipientId, `Alright! Thank you, bye now 🙏`);
     } else {
-      return listener(messageText, recipientId);
+      console.log(/[messageText]/i.test("show me restaurants in yaba nigeria"), "test vars")
+      if (/[keywords.show]/i.test(messageText)) {
+        return listener(messageText, recipientId);
+      } else {
+        return sendMessage(recipientId, `Sorry command on show is incorrect, please check page`);
+      }
     }
   }
 }
