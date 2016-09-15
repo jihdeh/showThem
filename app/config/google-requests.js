c480fc03b2517f9ef24bbf3b6033a28ca679c759
Jihdeh <jide.b.tade@gmail.com>
2016-09-15 18:17:29 +0100

    another logger

--- app/config/google-requests.js
+++ app/config/google-requests.js
@@ -11,6 +11,7 @@ async function googleRequestAPI(data) {
       params: { key: process.env.GOOGLE_API_KEY, query: data.query },
       method: "get"
     });
+    console.log("elllo", newResponse)
     const newResponse = response.data.results.splice(0, 10);
     if (newResponse.length > 0) {
       const endResponse = await getRequestImages(newResponse);
