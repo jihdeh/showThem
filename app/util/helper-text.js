eda39ffa64cf7c8a68a0c2e4b4661decce668101
Jihdeh <jide.b.tade@gmail.com>
2016-09-16 04:51:14 +0100

    brush up helper text

--- app/util/helper-text.js
+++ app/util/helper-text.js
@@ -1,3 +1,6 @@
-const text = `Hello there welcome to SHOWTHEMPLACES-BOT!`;
+const text = `✨Hello there welcome to SHOWTHEMPLACES-BOT!✨ \nYou can start by using commands such as 
+\nShow me {ANYWHERE}, for example: show me restaurants in yaba lagos nigeria. or 
+\nMap on {ANYWHERE}, map on the the white house
+`;
 
 module.exports = text;
