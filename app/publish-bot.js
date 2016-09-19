251ccebf711a26dfe97cc74092fe18e9100f002d
Jihdeh <jide.b.tade@gmail.com>
2016-09-19 19:45:55 +0100

    Revert

--- app/publish-bot.js
+++ app/publish-bot.js
@@ -43,7 +43,7 @@ async function receivedMessage(event) {
   const messageText = message.text;
   const messageAttachments = message.attachments;
   console.log(message, (message && message.is_echo));
-  if (messageText & (get(message, "is_echo") == undefined)) {
+  if (messageText) {
 
     // If we receive a text message, check to see if it matches any special
     // keywords and send back the corresponding example. Otherwise, just echo
