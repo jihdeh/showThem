2382f1a60c09aa110e570fc6ea1a0c46c529b84b
Jihdeh <jide.b.tade@gmail.com>
2016-09-19 18:26:56 +0100

    try this

--- app/publish-bot.js
+++ app/publish-bot.js
@@ -49,14 +49,13 @@ async function receivedMessage(event) {
     // keywords and send back the corresponding example. Otherwise, just echo
     // the text we received.
     switch (messageText) {
-      default: if (senderID) {
-        try {
-          await sendActions(senderID);
-        } catch (error) {
-          console.log(error)
-        }
-        sendTextMessage(senderID, messageText);
+      default: 
+      try {
+        await sendActions(senderID);
+      } catch (error) {
+        console.log(error)
       }
+      sendTextMessage(senderID, messageText);
     }
   } else if (messageAttachments) {
     console.log("message attachment received", messageAttachments);
@@ -86,14 +85,14 @@ function receivedPostback(event) {
 
   console.log("Received postback for user %d and page %d with payload '%s' " +
     "at %d", senderID, recipientID, payload, timeOfPostback);
-  switch (payload) {
+  switch(payload) {
     case "PAYLOAD_GETTING_STARTED":
       sendTextMessage(senderID, "help");
       break;
     case "PAYLOAD_LOCATION":
       sendTextMessage(senderID, "my location");
       break;
-    default:
+    default: 
       sendTextMessage(senderID, "help");
       break;
   }
